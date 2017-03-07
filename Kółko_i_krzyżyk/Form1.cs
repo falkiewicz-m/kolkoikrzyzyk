fd040ab93ae7e86553dfd9a3d530832eb28d35ad
Michał Falkiewicz <micfalk1@student.pg.gda.pl>
2017-03-07 06:28:56 +0100

    Add project files.

---
+++ Kółko_i_krzyżyk/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kółko_i_krzyżyk
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
